0424b0182ddd352d0fd1df6728fc020caabf12c7
hussinxx700 <hussinsobhy700@gmail.com>
2025-07-13 05:57:04 +0300

    Update SQL prompt in get_sql_database_chain for improved query generation and response formatting

--- database_test.py
+++ database_test.py
@@ -61,7 +61,21 @@ def get_example_selector() -> SemanticSimilarityExampleSelector:
 def get_sql_database_chain() -> SQLDatabaseChain:
     """Get the SQL database chain with few-shot learning."""
 
-    mysql_prompt = _mysql_prompt
+    mysql_prompt = """You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
+    Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+    Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
+    Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+    Pay attention to use CURDATE() function to get the current date, if the question involves "today".
+
+    Use the following format:
+
+    Question: Question here
+    SQLQuery: Query to run with no pre-amble
+    SQLResult: Result of the SQLQuery
+    Answer: Final answer here.
+
+    No pre-amble.
+    """
 
     example_prompt = PromptTemplate(
         input_variables=["Question", "SQLQuery", "SQLResult","Answer",],
@@ -92,7 +106,7 @@ def get_sql_database_chain() -> SQLDatabaseChain:
     return chain
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     """Main function to run the SQL database chain."""
     
     chain = get_sql_database_chain()
@@ -103,6 +117,6 @@ def get_sql_database_chain() -> SQLDatabaseChain:
     # Run the chain with the query
     response = chain.run(query)
     
-    print(response)'''
+    print(response)
 
    
