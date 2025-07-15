96b59a99adf08f49b0f35f665091c979dc5d18e3
hussinxx700 <hussinsobhy700@gmail.com>
2025-07-15 11:07:12 +0300

    Remove custom prefix from prompt template and adjust suffix for clarity in SQL query generation

--- custom_prompts.py
+++ custom_prompts.py
@@ -48,37 +48,13 @@ example_prompt= PromptTemplate(
     template="\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}",
 )
 
-
-
-custom_prefix= """
-    Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-
-    Unless the user specifies a specific number of results, limit your query to at most {top_k} rows using the LIMIT clause.
-
-    Never SELECT all columns. Only query the columns needed to answer the question.
-
-    Only use the column names and tables shown in the schema. Do not guess or invent columns or table names.
-
-    Use CURDATE() if the question refers to "today".
-
-    Use this exact format:
-
-    Here’s how to format your answer:
-
-    Question: How many white Nike t-shirts are in stock?
-    SQLQuery: SELECT sum(`stock_quantity`) FROM `t_shirts` WHERE `brand` = 'Nike' AND `color` = 'White'
-    SQLResult: [(Decimal('208'),)]
-    Answer: 208
-
-"""
-
 CUSTOM_suffix= """
     Only use the following tables:
 
     {table_info}
 
     Question: {input}
-    Answer:
+    SQLQuery:
 """
 
 
